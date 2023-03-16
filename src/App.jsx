dc3cd4d2eda703d75b8160683e31fbcb9698e187
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-03-16 21:18:05 +0700

    [CHORE] Add React Page Flip package

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,78 @@
-import { Route, Routes } from 'react-router-dom';
-import { MainMenu } from '@/pages';
+import { ButtonIcon } from '@/components/atoms';
+import clsx from 'clsx';
+import React, { useRef, useState } from 'react';
+
+import backsoundURL from '@/audios/bensound-cutesttoys.mp3';
+import HTMLFlipBook from 'react-pageflip';
+import { MainMenu } from './pages';
 
 function App() {
+	const backsoundRef = useRef(null);
+
+	const [isBacksoundReady, setIsBacksoundReady] = useState(false);
+	const [playing, setPlaying] = useState(false);
+
+	const IS_MUSIC_ON = playing && isBacksoundReady;
+
+	const toggleBacksound = () => {
+		if (backsoundRef.current?.paused) {
+			backsoundRef.current?.play();
+			setPlaying(true);
+		} else {
+			backsoundRef.current.pause();
+			setPlaying(false);
+		}
+	};
+
+	const onBacksoundReady = () => {
+		backsoundRef.current.volume = 0.5;
+		setIsBacksoundReady(true);
+	};
+
+	const onStartReading = () => {
+		console.log('START');
+	};
+
 	return (
-		<div>
-			<Routes>
-				<Route path="/" element={<MainMenu />} />
-			</Routes>
+		<div className="h-screen max-h-screen overflow-hidden">
+			<img
+				className="absolute top-0 left-0 object-cover w-full h-full opacity-50 -z-10"
+				src={require('@/images/background.jpg')}
+				alt=""
+			/>
+
+			<div className="container relative flex flex-col items-center justify-center h-full max-w-screen-xl gap-12 mx-auto top-5">
+				<HTMLFlipBook width={1024} height={650}>
+					<div>
+						<MainMenu onClickStart={onStartReading} />
+					</div>
+					<div>Page 2</div>
+					<div>Page 3</div>
+					<div>Page 4</div>
+				</HTMLFlipBook>
+			</div>
+
+			<div className="absolute bottom-5 right-8">
+				<div className="flex items-center justify-center">
+					<audio
+						ref={backsoundRef}
+						src={backsoundURL}
+						autoPlay
+						loop
+						onLoadedData={onBacksoundReady}
+						onPlay={() => setPlaying(true)}
+					/>
+
+					<div className="text-sm text-center">
+						<ButtonIcon
+							icon={require('@/images/symbol/music.png')}
+							className={clsx('w-12', !IS_MUSIC_ON && 'opacity-50')}
+							onClick={toggleBacksound}
+						/>
+						<div>{IS_MUSIC_ON ? 'ON' : 'OFF'}</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/pages/MainMenu/MainMenu.jsx
+++ src/pages/MainMenu/MainMenu.jsx
@@ -1,76 +1,18 @@
 import { ButtonIcon } from '@/components/atoms';
-import clsx from 'clsx';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 
-import backsoundURL from '@/audios/bensound-cutesttoys.mp3';
-
-export const MainMenu = () => {
-	const backsoundRef = useRef(null);
-
-	const [isBacksoundReady, setIsBacksoundReady] = useState(false);
-	const [playing, setPlaying] = useState(false);
-
-	const toggleBacksound = () => {
-		if (backsoundRef.current?.paused) {
-			backsoundRef.current?.play();
-			setPlaying(true);
-		} else {
-			backsoundRef.current.pause();
-			setPlaying(false);
-		}
-	};
-
-	useEffect(() => {
-		if (isBacksoundReady) {
-			backsoundRef.current?.play();
-		}
-	}, [isBacksoundReady]);
-
-	const isMusicOn = playing && isBacksoundReady;
-
+export const MainMenu = ({ onClickStart }) => {
 	return (
-		<div className="h-screen max-h-screen overflow-hidden">
-			<img
-				className="absolute top-0 left-0 object-cover w-full h-full opacity-50 -z-10"
-				src={require('@/images/background.jpg')}
-				alt=""
-			/>
-
-			<div className="container relative flex flex-col items-center justify-center h-full max-w-screen-xl gap-12 mx-auto top-5">
-				<div className="flex items-center justify-center w-full gap-10 rounded-lg">
-					<div className="relative w-full">
-						<img className="z-0 mx-auto rounded-lg shadow-xl" src={require('@/images/cover.jpg')} alt="" />
-					</div>
-
-					<div className="flex flex-col items-center justify-center w-full gap-10">
-						<img className="w-96" src={require('@/images/logo.png')} alt="" />
-
-						<div className="flex items-center justify-center w-full gap-3">
-							<ButtonIcon icon={require('@/images/symbol/start.png')} />
-						</div>
-					</div>
-				</div>
+		<div className="flex items-center justify-center w-full gap-10 rounded-lg">
+			<div className="relative w-full">
+				<img className="z-0 mx-auto rounded-lg shadow-xl" src={require('@/images/cover.jpg')} alt="" />
 			</div>
 
-			<div className="absolute bottom-5 right-8">
-				<div className="flex items-center justify-center">
-					<audio
-						ref={backsoundRef}
-						src={backsoundURL}
-						autoPlay
-						loop
-						onLoadedData={() => setIsBacksoundReady(true)}
-						onPlay={() => setPlaying(true)}
-					/>
+			<div className="flex flex-col items-center justify-center w-full gap-10">
+				<img className="w-96" src={require('@/images/logo.png')} alt="" />
 
-					<div className="text-sm text-center">
-						<ButtonIcon
-							icon={require('@/images/symbol/music.png')}
-							className={clsx('w-12', !isMusicOn && 'opacity-50')}
-							onClick={toggleBacksound}
-						/>
-						<div>{isMusicOn ? 'ON' : 'OFF'}</div>
-					</div>
+				<div className="flex items-center justify-center w-full gap-3">
+					<ButtonIcon icon={require('@/images/symbol/start.png')} onClick={onClickStart} />
 				</div>
 			</div>
 		</div>
