e043e99567e9f43f7602e460c2bd638f66fb64ef
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-03-16 21:51:54 +0700

    [CHORE] fix performance : complress or minify all story images

--- src/data/bookData.js
+++ src/data/bookData.js
@@ -3,55 +3,55 @@ export const BOOK_DATA = {
 	pages: [
 		{
 			number: 1,
-			image: require('@/images/story/1.jpg'),
+			image: require('@/images/story/1-min.jpg'),
 		},
 		{
 			number: 2,
-			image: require('@/images/story/2.jpg'),
+			image: require('@/images/story/2-min.jpg'),
 		},
 		{
 			number: 3,
-			image: require('@/images/story/3.jpg'),
+			image: require('@/images/story/3-min.jpg'),
 		},
 		{
 			number: 4,
-			image: require('@/images/story/4.jpg'),
+			image: require('@/images/story/4-min.jpg'),
 		},
 		{
 			number: 5,
-			image: require('@/images/story/5.jpg'),
+			image: require('@/images/story/5-min.jpg'),
 		},
 		{
 			number: 6,
-			image: require('@/images/story/6.jpg'),
+			image: require('@/images/story/6-min.jpg'),
 		},
 		{
 			number: 8,
-			image: require('@/images/story/8.jpg'),
+			image: require('@/images/story/8-min.jpg'),
 		},
 		{
 			number: 9,
-			image: require('@/images/story/9.jpg'),
+			image: require('@/images/story/9-min.jpg'),
 		},
 		{
 			number: 10,
-			image: require('@/images/story/10.jpg'),
+			image: require('@/images/story/10-min.jpg'),
 		},
 		{
 			number: 11,
-			image: require('@/images/story/11.jpg'),
+			image: require('@/images/story/11-min.jpg'),
 		},
 		{
 			number: 12,
-			image: require('@/images/story/12.jpg'),
+			image: require('@/images/story/12-min.jpg'),
 		},
 		{
 			number: 13,
-			image: require('@/images/story/13.jpg'),
+			image: require('@/images/story/13-min.jpg'),
 		},
 		{
 			number: 14,
-			image: require('@/images/story/14.jpg'),
+			image: require('@/images/story/14-min.jpg'),
 		},
 	],
 };
