3083c40a9fa21f7197bb5ed86dbab0b0a23f37d6
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-05-18 21:01:10 +0700

    [CHORE] handle condition for menus depends on current route

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,13 @@ function App() {
 
 	const { showSplashScreen } = useAppStore();
 
+	const { pathname } = location;
+
 	const exportRef = useRef();
 
-	const IS_AT_HOME = location.pathname === '/';
+	const IS_AT_HOME = pathname === '/';
+	const IS_AT_SPLASH = pathname === '/splash';
+	const IS_AT_USAGE_INSTRUCTION = pathname === '/usage-instruction';
 
 	return (
 		<div ref={exportRef} className="h-screen max-h-screen overflow-hidden">
@@ -32,7 +36,7 @@ function App() {
 
 			<div className="container h-full max-w-screen-xl mx-auto">
 				<AnimatePresence mode="wait">
-					<Routes location={location} key={location.pathname}>
+					<Routes location={location} key={pathname}>
 						<Route path="/" exact element={<MainMenu />} />
 						<Route path="/usage-instruction" element={<UsageInstruction />} />
 						<Route path="/author" element={<Author />} />
@@ -42,9 +46,9 @@ function App() {
 				</AnimatePresence>
 			</div>
 
-			{!showSplashScreen && (
+			{!IS_AT_SPLASH && (
 				<div className="absolute flex items-start gap-4 top-5 left-8">
-					{!IS_AT_HOME && <ButtonGoHome />}
+					{!IS_AT_HOME && !IS_AT_USAGE_INSTRUCTION && !IS_AT_SPLASH && <ButtonGoHome />}
 					{IS_AT_HOME && (
 						<Button icon={require('@/images/symbol/buku.png')} size="sm" onClick={() => navigate('/usage-instruction')}>
 							Petunjuk Penggunaan
@@ -53,7 +57,7 @@ function App() {
 				</div>
 			)}
 
-			{!showSplashScreen && (
+			{!IS_AT_SPLASH && (
 				<div className="absolute flex items-start gap-4 top-5 right-8">
 					<ButtonFullScreen />
 					<ButtonBacksound />
