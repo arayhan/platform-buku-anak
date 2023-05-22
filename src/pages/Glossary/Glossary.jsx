79715497e89b010c6dda4634e7272ab1a441ee04
Ahmed Rayhan Primadedas <rayhanprima99@gmail.com>
2023-05-23 01:41:52 +0700

    [CHORE] update responsive on glossary page

--- src/pages/Glossary/Glossary.jsx
+++ src/pages/Glossary/Glossary.jsx
@@ -8,12 +8,12 @@ export const Glossary = () => {
 
 	return (
 		<Fade>
-			<div className="w-full max-h-[92vh] px-20 py-16 mt-16 mb-8 space-y-10 overflow-y-scroll bg-white bg-opacity-50 rounded-lg shadow-lg">
+			<div className="w-full max-h-[92vh] px-4 md:px-10 lg:px-20 py-16 mt-16 mb-8 space-y-10 overflow-y-scroll bg-white bg-opacity-50 rounded-lg shadow-lg">
 				<div>
 					<h1 className="text-xl font-bold text-center">Glossary</h1>
 				</div>
 				<div>
-					<table id="glossary" className="text-left">
+					<table id="glossary" className="text-sm text-left md:text-base lg:text-lg">
 						<tr>
 							<th>Istimewa</th>
 							<td>:</td>
